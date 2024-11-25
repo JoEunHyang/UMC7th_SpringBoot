66d472b649cd5ef720ffa65363a8231b30e16a26
JoEunHyang <click2jeh@naver.com>
2024-11-25 18:01:40 +0900

    week7 - lombok 사용

--- src/main/java/umc/spring/dto/MissionDto.java
+++ src/main/java/umc/spring/dto/MissionDto.java
@@ -1,14 +1,17 @@
 package umc.spring.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import umc.spring.domain.enums.MissionStatus;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@ToString
 public class MissionDto {
     // Getter 및 Setter
     private Long memberMissionId;
@@ -24,29 +27,29 @@ public class MissionDto {
 
     // 생성자
 
-    public MissionDto(Long memberMissionId, Long missionId, MissionStatus missionStatus, String storeName, Integer missionReward, String missionSpec, Double storeScore, Long cursor, LocalDate deadline) {
-        this.memberMissionId = memberMissionId;
-        this.missionId = missionId;
-        this.missionStatus = missionStatus;
-        this.storeName = storeName;
-        this.missionReward = missionReward;
-        this.missionSpec = missionSpec;
-        this.cursor = cursor;
-        this.deadline = deadline;
-    }
+//    public MissionDto(Long memberMissionId, Long missionId, MissionStatus missionStatus, String storeName, Integer missionReward, String missionSpec, Double storeScore, Long cursor, LocalDate deadline) {
+//        this.memberMissionId = memberMissionId;
+//        this.missionId = missionId;
+//        this.missionStatus = missionStatus;
+//        this.storeName = storeName;
+//        this.missionReward = missionReward;
+//        this.missionSpec = missionSpec;
+//        this.cursor = cursor;
+//        this.deadline = deadline;
+//    }
 
 
-    @Override
-    public String toString() {
-        return "MissionDto{" +
-                "memberMissionId=" + memberMissionId +
-                ", missionId=" + missionId +
-                ", missionStatus=" + missionStatus +
-                ", storeName='" + storeName + '\'' +
-                ", missionReward=" + missionReward +
-                ", missionSpec='" + missionSpec + '\'' +
-                ", cursor=" + cursor +
-                ", deadline=" + deadline +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "MissionDto{" +
+//                "memberMissionId=" + memberMissionId +
+//                ", missionId=" + missionId +
+//                ", missionStatus=" + missionStatus +
+//                ", storeName='" + storeName + '\'' +
+//                ", missionReward=" + missionReward +
+//                ", missionSpec='" + missionSpec + '\'' +
+//                ", cursor=" + cursor +
+//                ", deadline=" + deadline +
+//                '}';
+//    }
 }
